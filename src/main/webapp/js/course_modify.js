21506fb2e6f31b3435e943d53bdb7c831e79500f
SharonLee <sharonlee@base.tw>
2017-01-17 13:56:39 +0800

    course_modify init

---
+++ src/main/webapp/js/course_modify.js
@@ -0,0 +1,11 @@
+
+var subjectAll = $('#subjectsID input[type="checkbox"]');
+var i;
+
+for(i = 0; i < subjectAll.length; i++) {
+    console.log(subjectAll[i].value);
+    var index = i + 1;
+    $('#subjectsID label:eq(' + index + ')').after('&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.w3schools.com" target="_blank">主題內容</a>');    
+}
+
+
